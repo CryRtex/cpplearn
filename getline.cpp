604973443bea4af7bc4f984fa8029924c25c9e3e
CryRtex <itspecialistt@protonmail.com>
2023-12-11 15:20:44 +0300

    Via learn getline method

---
+++ getline.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+ int main(){
+     
+     std::string name;  // variable definition
+     
+     
+     
+     
+     std::cout<<"Enter dick cm : ";
+     std::getline(std::cin, name); // full line get :D
+
+     if(name.length() > 12)
+     {
+        std::cout<<"Your name is too long"<<std::endl;
+     }
+     else{
+        std::cout<< "Your Name is too izi"<<std::endl;
+     }
+     
+      // ::empty
+     // ::clear
+    // ::append add some variable
+  // .at write console charachter
+//  .insert add some value inside character
+
+// .find find character in world
+// .erase some variable key deleted (0.3) etc
+    return 0;
+    }  // end of main function body    
