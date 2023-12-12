e3279f81fc6cbb6c948c93df2b3b45665d5dd8ac
CryRtex <itspecialistt@protonmail.com>
2023-12-12 14:29:59 +0300

    Add files via upload

---
+++ counter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+    int main(){
+
+       for(int x = 10; x > 0; x--){
+           std::cout<<x<<std::endl;
+           _sleep(1000); // sleep for 1 second
+         }
+         std::cout<<" HAPPY NEW YEAR"<<std::endl;
+
+
+
+
+        return 0;
+}
