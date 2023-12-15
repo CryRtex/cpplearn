bfe1698823061ea25ccdd1b8036e0b34119ca22c
CryRtex <itspecialistt@protonmail.com>
2023-12-15 20:48:57 +0300

    sizeof

---
+++ sizeof.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+int main(){
+    
+        std::cout << "Size of int: " << sizeof(int) << std::endl;
+        std::cout << "Size of double: " << sizeof(double) << std::endl;
+        std::cout << "Size of char: " << sizeof(char) << std::endl;
+        std::cout << "Size of bool: " << sizeof(bool) << std::endl;
+        std::cout << "Size of float: " << sizeof(float) << std::endl;
+        std::cout << "Size of long: " << sizeof(long) << std::endl;
+        std::cout << "Size of long long: " << sizeof(long long) << std::endl;
+        std::cout << "Size of long double: " << sizeof(long double) << std::endl;
+        std::cout << "Size of short: " << sizeof(short) << std::endl;
+        std::cout << "Size of unsigned int: " << sizeof(unsigned int) << std::endl;
+        std::cout << "Size of unsigned long: " << sizeof(unsigned long) << std::endl;
+        std::cout << "Size of unsigned long long: " << sizeof(unsigned long long) << std::endl;
+        std::cout << "Size of unsigned short: " << sizeof(unsigned short) << std::endl;
+        std::cout << "Size of unsigned char: " << sizeof(unsigned char) << std::endl;
+        std::cout << "Size of unsigned float: " << sizeof(unsigned float) << std::endl;
+        std::cout << "Size of unsigned double: " << sizeof(unsigned double) << std::endl;
+        std::cout << "Size of unsigned long double: " << sizeof(unsigned long double) << std::endl;
+        std::cout << "Size of unsigned bool: " << sizeof(unsigned bool) << std::endl;
+        std::cout << "Size of unsigned short: " << sizeof(unsigned short) << std::endl;
+        std::cout << "Size of unsigned int: " << sizeof(unsigned int) << std::endl;
+        std::cout << "Size of unsigned long: " << sizeof(unsigned long) << std::endl;
+        std::cout << "Size of unsigned long long: " << sizeof(unsigned long long) << std::endl;
+        std::cout << "Size of unsigned short: " << sizeof(unsigned short) << std::endl;
+        return 0;
+}
