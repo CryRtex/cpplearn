6ec7cb301af8ed2e1e43c10c824a147721aec7d0
CryRtex <itspecialistt@protonmail.com>
2024-01-08 16:47:37 +0300

    last out

---
+++ abs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a = 5;
+    int b = 87;
+
+    int difference = a + b;
+
+    std::cout << "The difference between " << b << " and " << a << " is " << difference << std::endl;
+
+    return 0;
+}
